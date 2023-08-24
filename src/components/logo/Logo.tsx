6e5be1cbfac4bb829614fdc8908cf598e0cf0b4c
Tedane Blake <tblake@ppvglobal.com>
2023-08-24 15:44:18 -0400

    logo update

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,12 +1,10 @@
 import { CheckCircledIcon } from '@radix-ui/react-icons';
+import logo from '@/assets/logo-full.svg';
 
 export const Logo = ({ size = 30 }) => {
   return (
-    <div className='flex items-center gap-2'>
-      <span className='bg-primary w-fit p-2 rounded-md'>
-        <CheckCircledIcon height={size} width={size} />
-      </span>
-      <span className='font-bold text-lg'>Taskify</span>
+    <div className='flex items-center my-2'>
+      <img src={logo} alt='logo' className='w-full object-contain h-9' />
     </div>
   );
 };
