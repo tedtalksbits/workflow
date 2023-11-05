0105d8a912197019d7e8bdf58534d0fa8c477a45
tedtalksbits <tedaneblake@gmail.com>
2023-11-05 00:56:04 -0400

    chore: remove log

--- src/hooks/useShortcuts.ts
+++ src/hooks/useShortcuts.ts
@@ -5,7 +5,6 @@ type UseShortcutsProps = (fn: (e: KeyboardEvent) => void) => void;
 export const useShortcuts: UseShortcutsProps = (fn) => {
   const handleShortcutOpen = useCallback(fn, [fn]);
   useEffect(() => {
-    console.log('shortcut effect ran');
     window.addEventListener('keydown', handleShortcutOpen);
     return () => {
       window.removeEventListener('keydown', handleShortcutOpen);
